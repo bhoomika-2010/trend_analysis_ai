0fa210d91f470c3cab4bcb54af92a13684c153e8
Bhoomika Anand <bhoomika.anand02@gmail.com>
2025-12-14 19:50:12 +0530

    for deploying

--- backend/app.py
+++ backend/app.py
@@ -777,4 +777,4 @@ def get_trend_forecast():
 if __name__ == '__main__':
     print("Starting Flask server...")
     print("Access the application at http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
